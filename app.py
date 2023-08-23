ddbf1d94fce9a1d2e3a429c03ef156ce8efeda12
Nived Krishna <100478612+NivedKris@users.noreply.github.com>
2023-08-24 03:37:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, Response
+import cv2
+from fer import FER
+
+app = Flask(__name__)
+detector = FER()
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        emotions = detector.detect_emotions(frame)
+        for face in emotions:
+            x, y, w, h = face['box']
+            emotion = max(face['emotions'], key=face['emotions'].get)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    cap.release()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
